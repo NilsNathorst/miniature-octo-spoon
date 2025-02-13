c73423bd98757af8210f54d0f516e73b2763b9af
Nils Nathorst-Windahl <38070556+NilsNathorst@users.noreply.github.com>
2025-02-13 14:30:04 +0100

    fix type

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,5 +1,5 @@
 export { getStartAndEndDate } from "./dateUtil";
-export { groupActivitiesByDay } from "./groupActivitiesByDay";
-export { getOrderOfWeekdays } from "./groupActivitiesByDay";
+export { groupActivitiesByDay } from "./group-activities-by-day";
+export { getOrderOfWeekdays } from "./group-activities-by-day";
 export { getAllClubs } from "./get-all-clubs";
 export { getActivities } from "./get-activities";
