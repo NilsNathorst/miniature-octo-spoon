e32abaab63c702836809635d77568fa68cc2ff94
Nils Nathorst-Windahl <38070556+NilsNathorst@users.noreply.github.com>
2025-02-16 21:18:01 +0100

    add Loading spinnger

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import styled from "styled-components";
+
+const Spinner = styled.div`
+  width: 16px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: 4px solid;
+  border-color: #000 #0000;
+  animation: l1 1s infinite;
+
+  @keyframes l1 {
+    to {
+      transform: rotate(0.5turn);
+    }
+  }
+`;
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <LoadingContainer>
+      <Spinner />
+    </LoadingContainer>
+  );
 };
